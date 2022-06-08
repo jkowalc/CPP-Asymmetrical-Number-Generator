a1e177be03ba2f3f3392463114da7082f0a1a5c4
Jan Kowalczewski <jan.kowalczewski.stud@pw.edu.pl>
2022-06-08 23:42:34 +0200

    Implement user input

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include "Generator/Generator.h"
 #include <iostream>
-
+std::map<int, float>& get_probabilities_from_user(std::map<int, float>& prob)
+{
+    float sum;
+    int num;
+    float percent;
+    while(sum<100)
+    {
+        std::cout<< "Enter the number: ";
+        std::cin>> num;
+        std::cout<< "Enter the probability (in %): ";
+        std::cin>> percent;
+        if(sum + percent > 100)
+        {
+            percent = 100 - sum;
+        }
+        sum += percent;
+        prob[num] = percent;
+    }
+    return prob;
+}
 int main()
 {
-    std::map<int, float> prob = {{1, 25.0}, {2, 25.0}, {3, 50.0}};
-    std::cout<<generate_asymmetrical_random_number(prob)<<"\n";
+    std::map<int, float> prob;
+    get_probabilities_from_user(prob);
+    std::cout<< "Randomly generated number: " << generate_asymmetrical_random_number(prob) << "\n";
 }
