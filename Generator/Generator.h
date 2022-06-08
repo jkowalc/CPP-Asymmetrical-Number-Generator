603f47ca588a74cda2b77580ffbd415cdb53cc66
Jan Kowalczewski <jan.kowalczewski.stud@pw.edu.pl>
2022-06-08 23:31:57 +0200

    First working version of number generation

--- Generator/Generator.h
+++ Generator/Generator.h
@@ -1,29 +1,32 @@
 #include <map>
 #include <cstdlib>
-
-template <typename T>
-T generate_asymmetrical_random_number(const std::map<T, float> probabilities)
-{
-    std::map<T, float> ranges = probabilities;
-    transform_probabilities_to_ranges<T>(ranges);
-    float random_float = std::rand();
-    for(auto it = probabilities.begin(); it != probabilities.end(); ++it)
-    {
-        if(random_float <= probabilities.second / 100)
-        {
-            return probabilities.first;
-        }
-    }
-}
+#include <time.h>
 
 template <typename T>
 std::map<T, float>& transform_probabilities_to_ranges(std::map<T, float>& probabilities)
 {
-    float curr_range;
+    float curr_range = 0.0;
     for(auto it = probabilities.begin(); it != probabilities.end(); ++it)
     {
         curr_range += it->second;
         probabilities[it->first] = curr_range;
     }
     return probabilities;
+}
+
+template <typename T>
+T generate_asymmetrical_random_number(const std::map<T, float> probabilities)
+{
+    std::map<T, float> ranges = probabilities;
+    transform_probabilities_to_ranges<T>(ranges);
+    std::srand(time(NULL));
+    float random_percent = (rand() % 10000) / 100;
+    for(auto it = ranges.begin(); it != ranges.end(); ++it)
+    {
+        if(random_percent <= it->second)
+        {
+            return it->first;
+        }
+    }
+    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "Generator/Generator.h"
+#include <iostream>
 
 int main()
 {
-
+    std::map<int, float> prob = {{1, 25.0}, {2, 25.0}, {3, 50.0}};
+    std::cout<<generate_asymmetrical_random_number(prob)<<"\n";
 }
